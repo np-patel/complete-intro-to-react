55334e07a25711c8528047e4540c2adca5c967b2
nehal <nepatel30@gmail.com>
2017-08-15 23:06:41 +1200

    added jsx formetting

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -5,18 +5,23 @@ import {render} from 'react-dom'
 const ce = React.createElement;
 
 const MyTitle = function(props) {
-  return ce('div', null, ce('h1', { style: { color: props.color } }, props.title));
+  // return ce('div', null, ce('h1', { style: { color: props.color } }, props.title));
+  return (
+  		<div>
+  			<h1 style={{color: props.color}}>{props.title}</h1>
+  		</div>
+  	);
 };
 
 const MyFirstComponent = function() {
-  return ce(
-    'div',
-    { id: 'my-first-component' },
-    ce(MyTitle, { title: 'Game of Thrones', color: 'YellowGreen' }),
-    ce(MyTitle, { title: 'Stranger Things', color: 'GreenYellow' }),
-    ce(MyTitle, { title: 'Rick and Morty', color: 'LimeGreen' }),
-    ce(MyTitle, { title: 'AIB', color: 'peru' })
+  return (
+  		<div id="MyFirstComponent">
+  			<MyTitle title = "Game of Thrones" color= "YellowGreen"/>
+  			<MyTitle title = "Stranger Things'" color= "GreenYellow"/>
+  			<MyTitle title = "Rick and Morty" color= "LimeGreen"/>
+  			<MyTitle title = "Eeast India Comedy" color= "peru"/>
+    </div>
   );
 };
 
-render(ce(MyFirstComponent), document.getElementById('app'));
+render(<MyFirstComponent/>, document.getElementById('app'));
