d8dc0b35e75401132cbeae62c45076374f375b28
nehal <nepatel30@gmail.com>
2017-08-20 18:34:01 +1200

    started new app - created Landing, Search and showcard componanent

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,27 +1,22 @@
-import React from 'react'
-import {render} from 'react-dom'
 
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Landing from './Landing';
+import Search from './Search';
 
-const ce = React.createElement;
+const FourOhFour = () => <h1>404</h1>;
 
-const MyTitle = function(props) {
-  // return ce('div', null, ce('h1', { style: { color: props.color } }, props.title));
-  return (
-  		<div>
-  			<h1 style={{color: props.color}}>{props.title}</h1>
-  		</div>
-  	);
-};
-
-const MyFirstComponent = function() {
-  return (
-  		<div id="MyFirstComponent">
-  			<MyTitle title = "Game of Thrones" color= "YellowGreen"/>
-  			<MyTitle title = "Stranger Things'" color= "GreenYellow"/>
-  			<MyTitle title = "AIB" color= "LimeGreen"/>
-  			<MyTitle title = "Eeast India Comedy" color= "peru"/>
+const App = () => (
+  <BrowserRouter>
+    <div className="app">
+      <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route path="/search" component={Search} />
+        <Route component={FourOhFour} />
+      </Switch>
     </div>
-  );
-};
+  </BrowserRouter>
+);
 
-render(<MyFirstComponent/>, document.getElementById('app'));
+render(<App />, document.getElementById('app'));
